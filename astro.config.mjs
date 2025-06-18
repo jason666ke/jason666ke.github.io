19151f9ebd423ab75ddf4f77e7ac7a25d094c82a
jasonKe <a773449752@163.com>
2025-06-18 10:59:47 +0800

    Change to Astron

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jason666ke.github.io',
+	base: '/blue-belt',
 	integrations: [mdx(), sitemap()],
 });
